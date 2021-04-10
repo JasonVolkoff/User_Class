4e62f934ab2e531559cc2a7266079929f0027529
JasonVolkoff <kashijason@gmail.com>
2021-04-10 02:25:52 -0700

    chained methods; added return self to methods

--- user.py
+++ user.py
@@ -17,10 +17,12 @@ class User:
     # takes an argument that is the amount of the deposit, then increments the balance by that amount
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
 
     # takes an argument that is the amount of the withdrawl, then deincrements the balance by that amount
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
 
     def display_user_balance(self):  # takes no argument, self is implied
         # Checks if the user is overdrafted
@@ -29,8 +31,9 @@ class User:
             print(f"{self.name}'s balance is: {self.account_balance}")
         else:
             print(f"{self.name} has overdrafted. Balance is: {self.account_balance}")
-
+        return self
     # takes 2 arguments: one for the user who receives the money, another for the amount
+
     def transfer_money(self, other_user, amount):
         # calls the withdrawl function for the user who is transferring; then calls the deposit function for the other user - both by the amount
         if User.counter > 1:
@@ -38,6 +41,7 @@ class User:
             User.make_deposit(other_user, amount)
         else:
             print("Cannot transfer: there is only one User instance.")
+        return self
 
 
 # Create three instances
@@ -45,24 +49,14 @@ john = User("John Doe", "johnDoe@doeDomain.com")
 jason = User("Jason Doe", "jasonDoe@doeDomain.com")
 julie = User("Julie Doe", "julieDoe@doeDoamin.com")
 # Make three deposits, one withdrawl, then display balance
-john.make_deposit(45)
-john.make_deposit(5)
-john.make_deposit(15)
-john.make_withdrawal(3)
-john.display_user_balance()
+john.make_deposit(45).make_deposit(5).make_deposit(
+    15).make_withdrawal(3).display_user_balance()
 # Second user makes 2 deposits, 2 withdrawls, then displays balance
-jason.make_deposit(100)
-jason.make_deposit(100)
-jason.make_withdrawal(5)
-jason.make_withdrawal(10)
-jason.display_user_balance()
+jason.make_deposit(100).make_deposit(100).make_withdrawal(
+    5).make_withdrawal(10).display_user_balance()
 # Third user makes 1 deposit, 3 withdrawls, then displays balance
-julie.make_deposit(100)
-julie.make_withdrawal(40)
-julie.make_withdrawal(50)
-julie.make_withdrawal(60)
-julie.display_user_balance()
+julie.make_deposit(100).make_withdrawal(40).make_withdrawal(
+    50).make_withdrawal(60).display_user_balance()
 # Second user transfers money to the third user, prints both balances
-jason.transfer_money(julie, 50)
-jason.display_user_balance()
+jason.transfer_money(julie, 50).display_user_balance()
 julie.display_user_balance()
